48a9c8714899056d8a746af64afcc452bee01374
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-04-09 08:34:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@
       rm xray.zip
       sudo chmod +x /usr/local/bin/xray    
 
-      wget ${GITHUB}main/service/xray_service.sh && ./xray_service.sh
+      wget ${GITHUB}service/xray_service.sh && ./xray_service.sh
 
 
 # Mengumpulkan informasi dari ipinfo.io
@@ -57,44 +57,7 @@
       wget -O /usr/local/bin/wireproxy ${HOSTING}/wireproxy
       chmod +x /usr/local/bin/wireproxy
 
-cat <<EOF > /etc/wireproxy.conf
-[Interface]
-PrivateKey = 4Osd07VYMrPGDtrJfRaRZ+ynuscBVi4PjzOZmLUJDlE=
-Address = 172.16.0.2/32, 2606:4700:110:8fdc:f256:b15d:9e5c:5d1/128
-DNS = 1.1.1.1, 1.0.0.1, 2606:4700:4700::1111, 2606:4700:4700::1001
-MTU = 1280
-
-[Peer]
-PublicKey = bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=
-AllowedIPs = 0.0.0.0/0
-AllowedIPs = ::/0
-Endpoint = engage.cloudflareclient.com:2408
-
-[Socks5]
-BindAddress = 127.0.0.1:40000
-EOF
-
-cat > /etc/systemd/system/wireproxy.service << END
-[Unit]
-Description=WireProxy for WARP
-After=network.target
-
-[Service]
-ExecStart=/usr/local/bin/wireproxy -c /etc/wireproxy.conf
-RestartSec=5
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-END
-check_success "Gagal membuat service untuk WireProxy."
-sudo systemctl daemon-reload
-sudo systemctl enable wireproxy
-sudo systemctl start wireproxy
-sudo systemctl restart wireproxy
-print_msg $YB "Instalasi selesai."
-sleep 3
-clear
+      wget ${GITHUB}service/wireproxy_conf.sh && ./wireproxy_conf.sh
 
 # Fungsi untuk mendeteksi OS
 detect_os() {
