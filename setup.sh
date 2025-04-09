35631778cc91d0a341b3784c272dc25e489a6dda
Sandhj <135236046+Sandhj@users.noreply.github.com>
2025-04-09 08:31:09 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,35 +34,9 @@
       sudo unzip -o xray.zip -d /usr/local/bin
       rm xray.zip
       sudo chmod +x /usr/local/bin/xray    
-      
-cat <<EOF > /etc/systemd/system/xray.service
-[Unit]
-Description=Xray Service
-Documentation=https://github.com/xtls
-After=network.target nss-lookup.target
 
-[Service]
-User=nobody
-Group=nogroup
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/local/bin/xray run -confdir /usr/local/etc/xray/config/
-RestartSec=5
-Restart=always
-StandardOutput=file:/var/log/xray/access.log
-StandardError=file:/var/log/xray/error.log
-SyslogIdentifier=xray
-LimitNOFILE=infinity
-OOMScoreAdjust=100
+      wget ${GITHUB}main/service/xray_service.sh && ./xray_service.sh
 
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable xray
-sudo systemctl start xray
 
 # Mengumpulkan informasi dari ipinfo.io
       curl -s ipinfo.io/city?token=f209571547ff6b | sudo tee /usr/local/etc/xray/city
